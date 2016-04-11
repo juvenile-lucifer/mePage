9dd7729e085e293f951b396ef72c48783045d04c
wangyang <wywangyang94@163.com>
2016-04-11 18:16:49 +0800

    test touch

--- meSwipe.js
+++ meSwipe.js
@@ -8,26 +8,20 @@
 		})(data));
 
 		function start(event){
-			// window.document.getElementsByClassName('box')[0].innerHTML = 'ss';
 			var touch  = event.targetTouches[0];
-			window.document.getElementsByClassName('testTa')[0].innerHTML = touch.pageX;
-			window.document.getElementsByClassName('testTa')[1].innerHTML = touch.pageX +'x';
-			window.document.getElementsByClassName('testTa')[2].innerHTML = touch.pageY;
-			window.document.getElementsByClassName('testTa')[3].innerHTML = touch.pageY + 'y';
 			data.x = touch.pageX;
 			data.y = touch.pageY;
 			data.id = touch.identifier;
-			window.document.getElementsByClassName('box')[1].innerHTML = 'sds';
 		}
 		function end(event){
-			window.document.getElementsByClassName('box')[2].innerHTML = 'ses';
 			var touch = event.targetTouches[0];
-			window.document.getElementsByClassName('box')[4].innerHTML = touch.pageX + '';
-			window.document.getElementsByClassName('box')[5].innerHTML = touch.pageY + '';
-			window.document.getElementsByClassName('box')[6].innerHTML = data.x + '';
-			window.document.getElementsByClassName('box')[7].innerHTML = data.y + '';
+			alert('success');
+			window.document.getElementsByClassName('testTa')[0].innerHTML = touch.pageX + 'TX';
+			window.document.getElementsByClassName('testTa')[1].innerHTML = touch.pageY + 'TY';
+			window.document.getElementsByClassName('testTa')[2].innerHTML = data.x + 'DX';
+			window.document.getElementsByClassName('testTa')[3].innerHTML = data.y + 'DY';
 			if(touch.identifier === data.id){
-				window.document.getElementsByClassName('box')[8].innerHTML = touch.identifier + '';
+				alert(touch.identifier);
 				if( (touch.pageX - data.x) >30){
 					// cells.right();
 					// cells.update
